16d1ec271541cd572f157a4d336e2142834eaa64
Kirill <14krabik14@gmail.com>
2021-10-17 22:26:47 +0300

    add docs and exhandler

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from math import exp, cos, pow
 
-n: int = int(input("Enter n > "))
-x: float = float(input("Enter x > "))
+""" 
+    Inputs: 
+    n: positive int
+    x: float or int
+"""
+check: bool = False
+while not check:
+    try:
+        n: int = int(input("Enter positive int n > "))
+        x: float = float(input("Enter x > "))
+        if n < 0:
+            raise ValueError
+        check = True
+    except ValueError:
+        print("Enter correct values")
 
 
 def multiplier(nn: int, xx: float) -> float:
+    """
+    :param nn: int, required
+    :param xx: float or int, required
+    :return: float, result of multiple loop
+    """
     p: float = - exp(cos(abs(xx)))
     for k in range(2, nn + 1):
         p *= ((k - 1) / k) - exp(cos(abs(k * xx)))
