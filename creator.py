7c64d80de92c675e7eb2f3806224316539e6091a
Kirill <14krabik14@gmail.com>
2021-10-21 00:39:01 +0300

    add comments

--- creator.py
+++ creator.py
@@ -1,5 +1,5 @@
 from random import uniform
 
 with open("test.txt", 'w') as f:
-    for i in range(100):
-        f.write(str(uniform(0, 100))[:5] + " ")
+    for i in range(100): # здесь 100 это количество цифр, которые будут записаны в файл
+        f.write(str(uniform(0, 100))[:5] + " ") # здесь (0, 100) - диапазон цифр, [:5] - до какого знака обрезать
